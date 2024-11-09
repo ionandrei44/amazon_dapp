f4c3a77099a23bd4f326929c6903204ff748c9ed
Andrei <44devtests@gmail.com>
2024-11-09 20:10:03 +0200

    add dependencies + smoke test

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,15 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappazon", () => {
-
-})
+  it("has a name", async () => {
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    const dappazon = await Dappazon.deploy(); // Added await here
+    await dappazon.deployed(); // Ensure the contract is deployed before calling any function
+    expect(await dappazon.name()).to.equal("Dappazon");
+  });
+});
